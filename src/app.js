09b605fe8c35d029c91d42838fca55cec29ee8ea
antonella <antonella.gutierrezrodriguez@pcc.edu>
2019-08-14 10:50:21 -0700

    rendering of header and images works

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ import Header from './Header.js';
 import Footer from './Footer.js';
 import animals from '../data/images.js';
 import ImageList from './ImageList.js';
+import FilterImages from './FilterImages.js';
 
 class App extends Component {
     onRender(dom) {
@@ -32,6 +33,30 @@ class App extends Component {
 
         const photoCards = dom.querySelector('#photo-cards');
         photoCards.appendChild(imageListDOM);
+
+        const filterAnimalProps = {
+            animals: animals,
+            onFilter: (animalKeyword) => {
+                let filteredAnimals;
+                if(animalKeyword === 'all') {
+                    filteredAnimals = animals;
+                }
+                else {
+                    filteredAnimals = animals.filter(animal => {
+                        return animal.keyword === animalKeyword;
+                    });
+                }
+
+                const updateProps = { animals: filteredAnimals };
+                imageList.update(updateProps);
+            }
+        };
+
+        const filterAnimals = new FilterImages(filterAnimalProps);
+        const filterAnimalsDOM = filterAnimals.renderDOM();
+
+        const filterSection = dom.querySelector('#filters');
+        filterSection.appendChild(filterAnimalsDOM);
     }
     
     renderHTML() {
